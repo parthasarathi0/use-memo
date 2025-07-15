c712471c847815fb06fe035443a6ae5bec362b13
parthasarathi0 <sarathisree001@gmail.com>
2025-07-15 22:07:59 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,43 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 const App = () => {
   const [number, setNumber] = useState(0);
-  const [dark, setDark] = useState(false);
-  const doublenum = useMemo(()=>{
-    return slow(number)
-  },[number])
-  
-  const colours = useMemo(()=>{
-    return  {
-    color: dark ? "white" : "#000",
-    backgroundColor: dark ? "#000" : "#fff",
+  const [dark, setDark] = useState(true);
+  // const doubNum = slowFun(number)
+const doubNum = useMemo(()=>{
+ return slowFun(number)
+},[number])
+  const colors = 
+  useMemo(()=>{
+return {
+    color: dark ?  "#000":"#fff",
+    backgroundColor: dark ? "#fff": "#000",
+    height: "100px",
     width: "100%",
-    height: "10px",
   }
   },[dark])
+  
+ useEffect(()=>{
+  console.log("theme changed");
+  
+ },[colors])
+
   return (
     <div>
-      <input type="number" onChange={(e) => setNumber(e.target.value)} />{" "}
-      <button onClick={() => setDark((curr) => !curr)}>Clickme</button>
-      <p>{doublenum}</p>
-      <div className="div" style={colours}></div>
+      <input type="number" onChange={(e) => setNumber( e.target.value)} />
+      <button onClick={()=>setDark((dark)=>!dark)}>toggle</button>
+      <div style={colors}>{doubNum}</div>
     </div>
   );
 };
 
 
 
-function slow(num) {
-  console.log(num);
-  
-  for(let i=0; i<=1000000000;i++){
+
+const slowFun = (num)=>{
+  for ( let i=0 ; i<1000000000 ; i++){
 
   }
-  console.log(num*2);
-  
-  return num * 2
+  return num*2
 }
-
 export default App;
